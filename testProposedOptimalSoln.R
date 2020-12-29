a01bd317a79c56ffb1971c434acd4a8601ab48c6
NAPTlME <napratt2@yahoo.com>
2020-12-29 13:09:54 -0600

    Create testProposedOptimalSoln.R
    Found additional optimal solutions, imported for comparison with the algorithm's choices.

---
+++ testProposedOptimalSoln.R
@@ -0,0 +1,225 @@
+# testing potions mentioned here: https://skyrimforums.org/sf/threads/discover-all-ingredient-effects-by-brewing-56-potions.18638/
+
+testPotText1 = "1. Creep Cluster + Dwarven Oil + Taproot
+2. Elves Ear + Fire Salts + White Cap
+3. Cyrodilic Spadetail + Jazbay Grapes + Salt Pile
+4. Dragon's Tongue + Fly Amanita + Scaly Pholiota
+5. Slaughterfish Egg + Slaughterfish Scales + Thistle Branch
+6. Abecean Longfin + Bleeding Crown + Ice Wraith Teeth
+7. Bee + Giant Lichen + Mora Tapinella
+8. Bleeding Crown + Tundra Cotton + Void Salts
+9. Blisterwort + Blue Mountain Flower + Hagraven Feathers
+10. Blue Butterfly Wing + Bone Meal + Snowberries
+11. Daedra Heart + Giant's Toe + Wheat
+12. Falmer Ear + Fly Amanita + Troll Fat
+13. Juniper Berries + Moon Sugar + Vampire Dust
+14. Canis Root + Falmer Ear + Spider Egg
+15. Chaurus Eggs + Lavender + Nirnroot
+16. Blisterwort + Hagraven Claw + Spriggan Sap
+17. Butterfly Wing + Hagraven Claw + Wheat
+18. Giant's Toe + River Betty + Salt Pile
+19. Bone Meal + Deathbell + Giant Lichen
+20. Canis Root + Imp Stool + Mora Tapinella
+21. Daedra Heart + Rock Warbler Egg + Scaly Pholiota
+22. Hagraven Claw + Lavender + White Cap
+23. Histcarp + Nordic Barnacle + Red Mountain Flower
+24. Ice Wraith Teeth + Luna Moth Wing + Vampire Dust
+25. Cyrodilic Spadetail + Daedra Heart + Nirnroot
+26. Dwarven Oil + Garlic + Slaughterfish Egg
+27. Canis Root + Juniper Berries + Rock Warbler Egg
+28. Beehive Husk + Ectoplasm
+29. Falmer Ear + Namira's Rot"
+
+testPotText2 = "30. Charred Skeever Hide + Eye of Sabre Cat + Mudcrab Chitin
+31. Glowing Mushroom + Pine Thrush Egg + Sabre Cat Tooth
+32. Blue Dartwing + Imp Stool + Swamp Fungal Pod
+33. Frost Mirriam + Orange Dartwing + Purple Mountain Flower
+34. Honeycomb + Silverside Perch + Skeever Tail
+35. Beehive Husk + Canis Root + Hawk Feathers
+36. Beehive Husk + Grass Pod + Red Mountain Flower
+37. Chaurus Eggs + Deathbell + Large Antlers
+38. Blue Dartwing + Namira's Rot + Nordic Barnacle
+39. Blue Mountain Flower + Glow Dust + Glowing Mushroom
+40. Abecean Longfin + Butterfly Wing + Small Antlers
+41. Chicken's Egg + Histcarp + Large Antlers
+42. Beehive Husk + Garlic + Luna Moth Wing
+43. Eye of Sabre Cat + Moon Sugar + Silverside Perch
+44. Honeycomb + Pearl + Thistle Branch
+45. Hawk Beak + Mudcrab Chitin + Snowberries
+46. Orange Dartwing + Slaughterfish Scales + Small Pearl"
+
+testPotText3 = "47. Bear Claws + Small Pearl + Hanging Moss
+
+48. Frost Salts + Hawk Beak + Pearl
+
+49. Briar Heart + Ectoplasm + Human Flesh
+
+50. Chicken's Egg + Nightshade + Wisp Wrappings
+
+51. Creep Cluster + Torchbug Thorax + Wisp Wrappings
+
+52. Abecean Longfin + Bee + Powdered Mammoth Tusk
+
+53. Cyrodilic Spadetail + Frost Salts + Powdered Mammoth Tusk
+
+54. Hagraven Feathers + Human Heart + Void Salts
+
+55. Human Flesh + Purple Mountain Flower + Torchbug Thorax
+
+56. Human Heart + River Betty + Spriggan Sap"
+
+testPotText4 = "57. Dragon's Tongue + Jazbay Grapes + Tundra Cotton
+58. Briar Heart + Slaughterfish Scales + White Cap
+59. Frost Salts + Lavender + Torchbug Thorax
+60. Blue Butterfly Wing + Pine Thrush Egg + Spider Egg
+61. Hawk Beak + Mora Tapinella + Scaly Pholiota
+62. Bee + Deathbell + Nightshade
+63. Elves Ear + Frost Mirriam + Juniper Berries
+64. Giant Lichen + Imp Stool + Skeever Tail
+65. Grass Pod + Red Mountain Flower + River Betty
+66. Creep Cluster + Swamp Fungal Pod + Wheat
+67. Nirnroot + Small Antlers + Small Pearl
+68. Butterfly Wing + Chaurus Eggs + Daedra Heart
+69. Eye of Sabre Cat + Glow Dust + Hanging Moss
+70. Blisterwort + Giant's Toe
+71. Hawk Feathers + Vampire Dust
+72. Orange Dartwing + Slaughterfish Egg
+73. Fire Salts + Moon Sugar
+74. Sabre Cat Tooth + Ice Wraith Teeth"
+
+testPotTextWiki = "Abecean Longfin + Bleeding Crown + Powdered Mammoth Tusk
+Abecean Longfin + Cyrodilic Spadetail + Giant's Toe
+Abecean Longfin + Human Flesh + White Cap
+Bear Claws + Eye of Sabre Cat + Hanging Moss
+Bee + Bone Meal + Fly Amanita
+Bee + Histcarp + Void Salts
+Beehive Husk + Ectoplasm + Grass Pod
+Beehive Husk + Hawk Feathers + Small Pearl
+Berit's Ashes + Bone Meal
+Bleeding Crown + Jazbay Grapes + Tundra Cotton
+Blisterwort + Blue Butterfly Wing + Spriggan Sap
+Blisterwort + Butterfly Wing + Hagraven Feathers
+Blue Butterfly Wing + Blue Mountain Flower + Wheat
+Blue Dartwing + Daedra Heart + Nordic Barnacle
+Blue Dartwing + Pine Thrush Egg + Spider Egg
+Bone Meal + Crimson Nirnroot + Lavender
+Briar Heart + Hagraven Claw + Tundra Cotton
+Briar Heart + Hawk Beak + Pearl
+Briar Heart + Purple Mountain Flower + Swamp Fungal Pod
+Butterfly Wing + Dragon's Tongue + Small Antlers
+Canis Root + Imp Stool + Rock Warbler Egg
+Canis Root + Namira's Rot + Spider Egg
+Charred Skeever Hide + Mudcrab Chitin + Swamp Fungal Pod
+Chaurus Eggs + Eye of Sabre Cat + Giant Lichen
+Chaurus Eggs + Garlic + Luna Moth Wing
+Chicken's Egg + Juniper Berries + Nordic Barnacle
+Chicken's Egg + Nightshade + Wisp Wrappings
+Creep Cluster + Dwarven Oil + Taproot
+Creep Cluster + Ice Wraith Teeth + Juniper Berries
+Creep Cluster + Mora Tapinella + Scaly Pholiota
+Crimson Nirnroot + Nirnroot + Void Salts
+Cyrodilic Spadetail + Jazbay Grapes + Salt Pile
+Cyrodilic Spadetail + Powdered Mammoth Tusk + Small Pearl
+Daedra Heart + Juniper Berries + Namira's Rot
+Deathbell + Salt Pile + Small Antlers
+Deathbell + Thistle Branch
+Dragon's Tongue + Dwarven Oil + Fly Amanita
+Dwarven Oil + Garlic + Slaughterfish Egg
+Ectoplasm + Red Mountain Flower + White Cap
+Elves Ear + Fire Salts + Juniper Berries
+Eye of Sabre Cat + Rock Warbler Egg + Torchbug Thorax
+Falmer Ear + Fly Amanita + Troll Fat
+Falmer Ear + Human Heart + Spider Egg
+Fire Salts + Frost Salts + Moon Sugar
+Frost Mirriam + Grass Pod + Spriggan Sap
+Frost Mirriam + Purple Mountain Flower + Silverside Perch
+Frost Salts + Giant Lichen + Hagraven Feathers
+Giant's Toe + Glow Dust + Glowing Mushroom
+Giant's Toe + River Betty + Skeever Tail
+Glow Dust + Human Heart + Nightshade
+Glowing Mushroom + Pine Thrush Egg + Sabre Cat Tooth
+Grass Pod + Large Antlers + River Betty
+Hagraven Claw + Snowberries + Swamp Fungal Pod
+Hawk Beak + Small Pearl + Wisp Wrappings
+Hawk Feathers + Luna Moth Wing + Vampire Dust
+Histcarp + Juniper Berries + Nordic Barnacle
+Honeycomb + Silverside Perch + Skeever Tail
+Honeycomb + Slaughterfish Scales + Thistle Branch
+Human Flesh + Imp Stool + Mora Tapinella
+Ice Wraith Teeth + Vampire Dust + White Cap
+Jarrin Root + Crimson Nirnroot + Human Heart
+Large Antlers + Torchbug Thorax + Wheat
+Lavender + Orange Dartwing + Slaughterfish Egg
+Mudcrab Chitin + Snowberries + Thistle Branch
+Orange Dartwing + Sabre Cat Tooth + Slaughterfish Scales"
+
+testPotText = paste(testPotText1, testPotText2, testPotText3, testPotText4, sep = "\n")
+
+
+testPotTextVec = str_split(testPotText, "\n")[[1]]
+
+testPotTextVecWiki = str_split(testPotTextWiki, "\n")[[1]]
+
+testPotList = lapply(testPotTextVec, function(x){
+   str_split(str_replace(x, "^\\d+\\.\\s*", ""), " \\+ ")[[1]]
+})
+testPotList = testPotList[sapply(testPotList, length) > 1]
+testPotList[[29]] = c(testPotList[[29]], "Luna Moth Wing")
+
+testPotListWiki = lapply(testPotTextVecWiki, function(x){
+  str_split(str_replace(x, "^\\d+\\.\\s*", ""), " \\+ ")[[1]]
+})
+testPotListWiki = lapply(testPotListWiki, function(x){
+  str_replace(x, "Berit's Ashes", "Berit's Ashes\nidentical to Bone Meal")
+})
+
+V(alchemyGraph)$Count = 50
+testGraph = induced_subgraph(alchemyGraph, V(alchemyGraph)[V(alchemyGraph)$Type == "Effect" | V(alchemyGraph)$name %in% unique(unlist(testPotList))])
+
+testGraphWiki = induced_subgraph(alchemyGraph, V(alchemyGraph)[V(alchemyGraph)$Type == "Effect" | V(alchemyGraph)$name %in% unique(unlist(testPotListWiki))])
+
+
+testPotReveal = getEffectRevealMetaData(testPotList, testGraph)
+length(testPotList)
+sum(testPotReveal$numRevealedEffects)
+summary(testPotReveal$numRevealedEffects)
+
+testPotWikiReveal = getEffectRevealMetaData(testPotListWiki, testGraphWiki)
+length(testPotListWiki)
+sum(testPotWikiReveal$numRevealedEffects)
+summary(testPotWikiReveal$numRevealedEffects)
+
+which(testPotWikiReveal$numRevealedEffects == max(testPotWikiReveal$numRevealedEffects))
+testPotListWiki[which(testPotWikiReveal$numRevealedEffects == max(testPotWikiReveal$numRevealedEffects))]
+
+startTime = Sys.time()
+runTmp = potionsRecommendedForEffectReveal1.1(testGraph)
+difftime(Sys.time(), startTime) # ~ 24 min
+runTmpReveal = getEffectRevealMetaData(runTmp, testGraph)
+length(runTmp)
+sum(runTmpReveal$numRevealedEffects)
+summary(runTmpReveal$numRevealedEffects)
+
+startTime = Sys.time()
+runTmp2 = potionsRecommendedForEffectReveal2(testGraph)
+difftime(Sys.time(), startTime) # ~ 24 min
+runTmpReveal2 = getEffectRevealMetaData(runTmp2, testGraph)
+length(runTmp2)
+sum(runTmpReveal2$numRevealedEffects)
+summary(runTmpReveal2$numRevealedEffects)
+
+startTime = Sys.time()
+runTmpWiki = potionsRecommendedForEffectReveal1.1(testGraphWiki)
+difftime(Sys.time(), startTime) # ~ 24 min
+runTmpRevealWiki = getEffectRevealMetaData(runTmpWiki, testGraphWiki)
+length(runTmpWiki)
+sum(runTmpRevealWiki$numRevealedEffects)
+summary(runTmpRevealWiki$numRevealedEffects)
+
+startTime = Sys.time()
+runTmpWiki2 = potionsRecommendedForEffectReveal1.1(testGraphWiki)
+difftime(Sys.time(), startTime) # ~ 24 min
+runTmpRevealWiki2 = getEffectRevealMetaData(runTmpWiki2, testGraphWiki)
+length(runTmpWiki2)
+sum(runTmpRevealWiki2$numRevealedEffects)
+summary(runTmpRevealWiki2$numRevealedEffects)
